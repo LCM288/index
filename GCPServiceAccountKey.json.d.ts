70f6490a094d5f9a57d565fb430f3fdd2e1067dd
LCM288 <charlielicmli@gmail.com>
2021-11-25 21:53:21 +0800

    use raw file name not file data for cert

--- GCPServiceAccountKey.json.d.ts
+++
@@ -1,5 +0,0 @@
-import type { ServiceAccount } from "firebase-admin/app";
-
-declare const serviceAccount: ServiceAccount;
-
-export default serviceAccount;
--- apollo/server/store.ts
+++ apollo/server/store.ts
@@ -1,16 +1,10 @@
 import { initializeApp, applicationDefault, cert } from "firebase-admin/app";
-import { getFirestore, Firestore } from "firebase-admin/firestore";
-import type { ServiceAccount } from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore";
 
-export const firestore = new Promise<Firestore>(async (resolve) => {
-  if (process.env.IS_GCP) {
-    initializeApp({ credential: applicationDefault() });
-  } else {
-    initializeApp({
-      credential: cert(
-        (await import("GCPServiceAccountKey.json")) as ServiceAccount
-      ),
-    });
-  }
-  resolve(getFirestore());
+initializeApp({
+  credential: process.env.IS_GCP
+    ? applicationDefault()
+    : cert("GCPServiceAccountKey.json"),
 });
+
+export const firestore = getFirestore();
